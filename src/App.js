595c03e4e3e1a3d4121d67e753b66f065384475c
jeff-moorhead <jeff.moorhead1@gmail.com>
2023-09-07 16:31:42 -0400

    add TODOs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
+// TODO: add basic search functionality
+// TODO: add randomizer (how to get a list of available books and verses?)
+
 function App() {
 
     const [ verse, setVerse ] = useState('')
     const fetchVerseData = () => {
-        const url = "https://bible-api.com/matthew+9:9-13"
+        const url = "https://bible-api.com/matthew+9:9-13?translation=kjv"
         fetch(url, {
             method: 'GET',
             accepts: 'application/json',
