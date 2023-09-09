d3dfdad1838fef6ea251efcc8f8dd83d6f5e2b3a
jeff-moorhead <jeff.moorhead1@gmail.com>
2023-09-09 09:55:10 -0400

    Added half of Genesis and updated README.Md

--- README.md
+++ README.md
@@ -5,3 +5,18 @@ This is a React frontend on top of https://bible-api.com. The app currently only
 Visit https://www.kingjamesbibleverses.com to see the app in action.
 
 It currently only has a few books and chapters. I will be adding more over time.
+
+## Why the Bible?
+The Bible has a few properties that made it an interesting candidate for this project. The primary reason I
+chose it is the unique layout of books, chapters, and verses. This added a challenge for me of figuring out
+how to populate three different `<Select>` components dynamically, since each book has a different number of
+chapters, and each chapter a different number of verses.
+
+I chose the King James Version because it's the only one I knew of the translations supported by www.bible-api.com
+and also sounds the most profound when quoted (in my opinion).
+
+## What else did I get out of this project?
+I got some good practice in managing DNS and setting up some basic infrastructure (although Digital Ocean handles
+almost everything automatically, which is really nice). The primary benefit I got out of this project was learning
+how to set up a slightly more complex React application with some interesting component positioning and dynamically
+populated `<Select>` components.
--- src/books.js
+++ src/books.js
@@ -25,6 +25,137 @@ const Books = [
                 maxVerse: 41,
             }
         ]
+    },
+    {
+        label: "Genesis",
+        value: "Genesis",
+        chapters: [
+            {
+                label: "Chapter 1",
+                value: 1,
+                maxVerse: 31,
+            },
+            {
+                label: "Chapter 2",
+                value: 2,
+                maxVerse: 25,
+            },
+            {
+                label: "Chapter 3",
+                value: 3,
+                maxVerse: 24,
+            },
+            {
+                label: "Chapter 4",
+                value: 4,
+                maxVerse: 26,
+            },
+            {
+                label: "Chapter 5",
+                value: 5,
+                maxVerse: 31,
+            },
+            {
+                label: "Chapter 6",
+                value: 6,
+                maxVerse: 22,
+            },
+            {
+                label: "Chapter 7",
+                value: 7,
+                maxVerse: 24,
+            },
+            {
+                label: "Chapter 8",
+                value: 8,
+                maxVerse: 22,
+            },
+            {
+                label: "Chapter 9",
+                value: 9,
+                maxVerse: 29,
+            },
+            {
+                label: "Chapter 10",
+                value: 10,
+                maxVerse: 32,
+            },
+			{
+                label: "Chapter 11",
+                value: 11,
+                maxVerse: 32,
+            },
+            {
+                label: "Chapter 12",
+                value: 12,
+                maxVerse: 20,
+            },
+            {
+                label: "Chapter 13",
+                value: 13,
+                maxVerse: 18,
+            },
+            {
+                label: "Chapter 14",
+                value: 14,
+                maxVerse: 24,
+            },
+            {
+                label:  "Chapter 15",
+                value: 15,
+                maxVerse: 21,
+            },
+            {
+                label: "Chapter 16",
+                value: 16,
+                maxVerse: 16,
+            },
+            {
+                label: "Chapter 17",
+                value: 17,
+                maxVerse: 27,
+            },
+            {
+                label: "Chapter 18",
+                value: 18,
+                maxVerse: 33,
+            },
+            {
+                label: "Chapter 19",
+                value: 19,
+                maxVerse: 38,
+            },
+            {
+                label: "Chapter 20",
+                value: 20,
+                maxVerse: 18,
+            },
+            {
+                label: "Chapter 21",
+                value: 21,
+                maxVerse: 34,
+            },
+            {
+                label: "Chapter 22",
+                value: 22,
+                maxVerse: 24,
+            },
+            {
+                label: "Chapter 23",
+                value: 23,
+                maxVerse: 20,
+            },
+            {
+                label: "Chapter 24",
+                value: 24,
+                maxVerse: 67,
+            },
+            {
+                label: "Chapter 25",
+                value: 25,
+                maxVerse: 34,
+            },
+        ]
     }
 ]
 
